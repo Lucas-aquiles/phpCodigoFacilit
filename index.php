8e94b087b233d64a7d06d3968f15c4e4acb1982b
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-05-30 17:48:35 -0300

    add

--- index.php
+++ index.php
@@ -12,3 +12,59 @@ $Nombre = "lucas";
 echo $nombre ," ", $edad, " " , $promedio , " ", $Nombre, escuela , direccion;
 
 ?>
+
+
+<?php 
+$data = "datazos";
+echo gettype($data);
+$data = 9.5;
+echo gettype($data);
+
+?>
+
+<?php
+$palabra = "buenas";
+$facilito = "tardes";
+$tardecito = "noches";
+
+//concatenar string
+echo $palabra.$facilito;
+//interpolacion string
+echo "hola $palabra $facilito";
+// con llaves
+echo "hola {$palabra} gente";
+
+
+?> 
+
+<!-- comillas  -->
+<?php 
+$saludo =  "hola";
+
+echo " $saludo Lucas Bienvenido";
+//si no usamos variables mejor usar comillas simples, ya q el interprete no las esta leyendo 
+echo ' $saludo Lucas Bienvenido';
+
+//comillas dobles
+echo "lucas \"dijo\" q se va de casa"
+?>
+
+<!-- heredoc -->
+
+<?php 
+$saludo = "buenas";
+echo <<<Frase
+$saludo
+No cuentas con los dias nublados paapaa
+Frase;
+?>
+
+<!-- nowdoc -->
+<?php 
+$saludo = "buenas";
+$frase= <<<'Frase'
+$saludo
+No cuentas con los dias nublados paapaa
+Frase;
+echo $frase;
+?>
