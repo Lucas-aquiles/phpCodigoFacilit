0274bc12b0817ee7e163644a33e8c87b77f0f6e6
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-05-30 01:13:46 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php  
+// print "Hola Mundo";
+// echo "hola Mundito";
+$nombre = "hola mama";
+const escuela = "Maria Jose";
+define("direccion","rio limay");
+$edad =  20;
+$promedio = 2.5;
+$esAlumno = true;
+$Nombre = "lucas";
+
+echo $nombre ," ", $edad, " " , $promedio , " ", $Nombre, escuela , direccion;
+
+?>
